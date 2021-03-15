77c1d751c230bad33a5015460180ee79915401ca
AleksDrumov <aleksdrumov.com@gmail.com>
2021-03-15 11:44:29 +0200

    first commit

---
+++ Grop_friends_1.py
@@ -0,0 +1,3 @@
+Grop_friends = ['Ярослав','Катя','Настя','Лілія','Олег']
+print(Grop_friends)
+print(Grop_friends[0])
---
+++ Group_friends_3.py
@@ -0,0 +1,3 @@
+Group_friends = ['Ярослав','Катя','Настя','Лілія','Олег']
+Group_friends.append('Сергій')
+print(Group_friends)
